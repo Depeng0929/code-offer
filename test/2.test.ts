40213049de867709569db6dc4eaf611e2d71061e
depeng.kong <15365182986@163.com>
2022-01-06 17:30:36 +0800

    feat: 2

--- test/2.test.ts
+++ test/2.test.ts
@@ -4,12 +4,36 @@ import { it,expect,describe } from "vitest"
 
 function find(arr: number[][], n: number) {
 
+  let rows = arr.length
+  let cols = rows && arr[0].length
+  if (rows > 0 && cols > 0) { 
+
+    let row = 0;
+    let col = cols-1;
+
+    while (row < rows && col > 0) {
+      if (arr[row][col] === n) {
+        return true
+      } else if (arr[row][col] > n) {
+        col--
+      } else {
+        row++
+      }
+    }
+  }
+  return false
+
 }
 
 describe('find', () => {
   it('[]', () => {
     expect(find([], 1)).toEqual(false)
     expect(
-      find([[1, 2, 7, 8, 9], [2, 4, 6, 8, 10], [3, 5, 7, 9, 11], [1, 3, 5, 7, 9], [2, 4, 6, 8, 10]], 8)).toEqual(true)
+      find([[1, 2, 7, 8, 9], [2, 4, 6, 8, 10], [3, 5, 7, 9, 11], [1, 3, 5, 7, 9], [2, 4, 6, 8, 10]], 8)).toBeTruthy()
+    
+      expect(
+        find([[1, 2, 7, 8, 9], [2, 4, 6, 8, 10], [3, 5, 7, 9, 11], [1, 3, 5, 7, 9], [2, 4, 6, 8, 10]], 20)).toBeFalsy()
   })
+
+  
 })
